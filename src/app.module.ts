1e202727ef2477975b376af312f77d6f98a499ea
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-03 18:00:12 +0300

    feat: init modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServicesModule } from './services/services.module';
+import { UsersModule } from './users/users.module';
+import { ResourcesModule } from './resources/resources.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [],
+  imports: [ ServicesModule, UsersModule, ResourcesModule, TasksModule, ArticlesModule ],
   controllers: [AppController],
   providers: [AppService],
 })
