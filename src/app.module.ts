578b8c98b600fa5ad299e673003906046fc8c780
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-13 18:51:00 +0300

    feat: examples module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServicesModule } from './services/services.module';
 import { UsersModule } from './users/users.module';
-import { ResourcesModule } from './resources/resources.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ArticlesModule } from './articles/articles.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SeoparserModule } from './seoparser/seoparser.module';
 import { EcomparserModule } from './ecomparser/ecomparser.module';
+import { ExamplesModule } from './examples/examples.module';
 
 @Module({
   imports: [ 
     MongooseModule.forRoot('mongodb://0.0.0.0:27017/iparseapidb'),
     ServicesModule, 
     UsersModule, 
-    ResourcesModule, 
     TasksModule, 
-    ArticlesModule, SeoparserModule, EcomparserModule 
+    ArticlesModule, SeoparserModule, EcomparserModule, ExamplesModule 
   ],
-  controllers: [AppController ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
