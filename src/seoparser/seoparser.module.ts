40befb53d415988543f28aae311247ea76c6fa73
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-15 13:19:02 +0300

    feat: service

--- src/seoparser/seoparser.module.ts
+++ src/seoparser/seoparser.module.ts
@@ -3,6 +3,7 @@ import { SeoparserController } from './seoparser.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SeoParserSchema } from './model/seoparser.model';
 import { SeoParserTaskSchema } from './model/seoparser.task.model';
+import { SeoparserService } from './seoparser.service';
 
 @Module({
   imports: [
@@ -18,5 +19,6 @@ import { SeoParserTaskSchema } from './model/seoparser.task.model';
     ]),
   ],
   controllers: [SeoparserController],
+  providers: [SeoparserService],
 })
 export class SeoparserModule {}
