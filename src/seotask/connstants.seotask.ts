8b78778380eb9015f76ec32c56af2d0d9a731ec2
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-08-23 16:35:12 +0300

    feat: controller for tasks

---
+++ src/seotask/connstants.seotask.ts
@@ -0,0 +1,1 @@
+export const SEO_TASK_NOT_FOUND_ERROR = 'Seo task was not found';
--- src/seotask/seotask.service.ts
+++ src/seotask/seotask.service.ts
@@ -5,6 +5,8 @@ import { SEO_PARSER_NOT_FOUND_ERROR } from 'src/seoparser/seoparser.constants';
 import { InjectModel } from '@nestjs/mongoose';
 import { SeoTaskDocument } from './model/seotask.model';
 import { Model } from 'mongoose';
+import { SEO_TASK_NOT_FOUND_ERROR } from './connstants.seotask';
+import { FinishSeoTaskDto } from './dto/finish-seotask.dto';
 
 @Injectable()
 export class SeotaskService {
@@ -31,4 +33,50 @@ export class SeotaskService {
     return newTask._id;
 
   }
+
+  async getVerifiedTask(id: string, userEmail: string): Promise<SeoTaskDocument | undefined> {
+
+    const taskFound = await this.seoTaskModel.findById(id).exec();
+
+    if (!taskFound) {
+      throw new NotFoundException(SEO_TASK_NOT_FOUND_ERROR);
+    }
+
+    const seoParser = await this.seoParserService.getVerifiedParser(taskFound.parser.toString(), userEmail);
+
+    if (!seoParser) {
+      throw new NotFoundException(SEO_TASK_NOT_FOUND_ERROR)
+    }
+
+   return taskFound;
+ 
+  }
+
+  async getTaskListByParser(parserid: string, userEmail: string) {
+    const seoParser = await this.seoParserService.getVerifiedParser(parserid, userEmail);
+    
+    if (!seoParser) {
+      throw new NotFoundException(SEO_PARSER_NOT_FOUND_ERROR)
+    }
+
+    return this.seoTaskModel.find({
+      parser: parserid,
+    }).exec();
+
+  }
+
+
+  async finishTask(dto: FinishSeoTaskDto, id: string, userEmail: string ) {
+
+
+    // тут нужно проверить, что user - имеет права ПАРСЕРА. юзер это не владелец ресурса, это парсер
+
+    return this.seoTaskModel.findByIdAndUpdate(id, {
+      ...dto,
+      finish: Date.now()
+    }, {new: true}).exec();
+
+
+  }
+
 }
